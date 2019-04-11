a0de19db6cb495a4f695ad3d6353050febcf2e5f
Praveen Developer <praveen@developer.org>
2019-04-10 21:09:12 -0400

    fixing bug in brach1 code

--- Example.java
+++ Example.java
@@ -2,6 +2,6 @@ class Example {
  public static void main(String args[]){
       	System.out.println("Debug message");
 	System.out.println("Hello World!");
-	System.out.println("branch1 changes");
+	System.out.println("branch1 changes"); 
    }
 }
